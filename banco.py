a8447b3129e16a1e37e49a76ee2c5505404b08d5
Felipe Cornélio Leite <felipecornelio@outlook.com.br>
2026-03-24 20:49:28 +0000

    inserir feito

--- banco.py
+++ banco.py
@@ -4,7 +4,7 @@ path = "banco.json"
 
 def abrir_banco():
     with open(path, "r", encoding="utf-8") as caminho:
-        banco = json.load(caminho, "utf8");
+        banco = json.load(caminho);
         return banco;
 
 def inserir(json_obj):
@@ -14,10 +14,15 @@ def inserir(json_obj):
     with open(path, "w", encoding="utf-8") as caminho:
         json.dump(t, caminho, indent=4, ensure_ascii=False);
         
-        
-def pesquisar(string):
-    banco = abrir_banco;
-    
 
-# def atualizar()
-# def deletar()
+#TODO Ansur
+def pesquisar(string):
+    banco = abrir_banco();
+
+#TODO Miguel
+def atualizar():
+    banco = abrir_banco();
+
+#TODO Miggs
+def deletar():
+    banco = abrir_banco();
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import banco
+import datetime
 
 print("+--------------------+")
 print("| 1. Cadastrar peças |")
@@ -7,18 +8,44 @@ print("| 3. Deletar peças   |")
 print("| 4. Inserir peças   |")
 print("+--------------------+")
 
-opt = input();
+peca = {"id":0,
+        "peca":"",
+        "tipo":"",
+        "parte":"",
+        "veiculos":[""],
+        "fabricante":"",
+        "data_fabricacao":""
+        }
 
+opt = int(input());
+
+
+#TODO Lucas
 while True:
     match(opt):
         case 1:
-            
+            print("Insira as informações da peça: ");
+            peca["peca"] = input("Qual a peça? ")
+            peca["tipo"] = input("De qual tipo é a peca? ")
+            peca["parte"] = input("De qual parte é a peça? ")
+            lista = []
+            while True:
+                lista.append(input("Para quais carros é a peça? (-1 para parar) "))
+                if lista[-1] == "-1":
+                    lista.pop()
+                    break;
+            peca["veiculos"] = lista
+            peca["fabricante"] = input("Qual o fabricante da peça? ")
+            ano = int(input("Ano de fabricação: "))
+            mes = int(input("Mes de fabricação: "))
+            dia = int(input("Dia de fabricação: "))
+            peca["data_fabricacao"] = datetime.datetime(ano,mes,dia).strftime("%x")
+
+            banco.inserir(peca)
             break
         case 2:
             break
         case 3:
             break
         case 4:
-            break
-        case _:
             break
