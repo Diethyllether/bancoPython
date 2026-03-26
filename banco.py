b56a5c13a9f1784f1851987a176da909075bed62
Miguel Seleme <22401032@aluno.cotemig.com.br>
2026-03-26 14:59:47 -0300

    atualizar feito

--- banco.py
+++ banco.py
@@ -20,13 +20,16 @@ def pesquisar(string):
     banco = abrir_banco();
 
 #TODO Miguel
-def atualizar(id_atualizar, novo_nome, novo_tipo, nova_parte):
+def atualizar(id_atualizar, novo_nome, novo_tipo, nova_parte, novo_veiculo, nova_fabricante, nova_data):
     banco = abrir_banco()
     for p in banco["pecas"]:
-        if p["id"] == id_atualizar:
+        if int(p["id"]) == int(id_atualizar):
             p["peca"] = novo_nome
             p["tipo"] = novo_tipo
             p["parte"] = nova_parte
+            p["veiculos"] = novo_veiculo
+            p["fabricante"] = nova_fabricante
+            p["data_fabricacao"] = nova_data
             with open(path, "w", encoding="utf-8") as caminho:
                 json.dump(banco, caminho, indent=4, ensure_ascii=False)
             print(f"Peça ID {id_atualizar} atualizada com sucesso!")
