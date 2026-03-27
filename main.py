52628ae40fccab1f8ef47c4fa6723b6c27f8e0a6
braga493 <22401725@aluno.cotemig.com.br>
2026-03-27 09:25:09 -0300

    mexi no menu

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ while True:
     print("| 1. Cadastrar peças |")
     print("| 2. Pesquisar peças |")
     print("| 3. Deletar peças   |")
-    print("| 4. Inserir peças   |")
+    print("| 4. Atualiazar peças|")
     print("| -1. para sair      |")
     print("+--------------------+")
 
@@ -43,12 +43,33 @@ while True:
         peca["data_fabricacao"] = datetime.datetime(ano,mes,dia).strftime("%x")
         banco.inserir(peca)
     if opt ==2:
-        id_pesquisa = int(input("digite o id da peça que você gostaria de ver:"))
-        banco.pesquisar(id_pesquisa) 
-        break;
+         print("Insira as informações da peça: ")
+         peca["id"]= input("Qual o ID da peça?")
+         banco.pesquisar(peca["id"])
+
     if opt == 3:
         id_pesquisa = int(input("digite o id da peça que você gostaria de deletar:"))
         banco.deletar(id_pesquisa)
-        break;
+    
+    if opt == 4:
+        print("Insira as informações da peça: ");
+        peca["id"]= input("Qual o ID da peça?")
+        peca["peca"] = input("Qual a peça? ")
+        peca["tipo"] = input("De qual tipo é a peca? ")
+        peca["parte"] = input("De qual parte é a peça? ")
+        lista = []
+        while True:
+            lista.append(input("Para quais carros é a peça? (-1 para parar) "))
+            if lista[-1] == "-1":
+                lista.pop()
+                break;
+        peca["veiculos"] = lista
+        peca["fabricante"] = input("Qual o fabricante da peça? ")
+        ano = int(input("Ano de fabricação: "))
+        mes = int(input("Mes de fabricação: "))
+        dia = int(input("Dia de fabricação: "))
+        peca["data_fabricacao"] = datetime.datetime(ano,mes,dia).strftime("%x")
+        banco.atualizar(peca["id"],peca["peca"],peca["tipo"],peca["parte"],peca["fabricante"],peca["data_fabricacao"])
+      
     if opt==-1:
          break;
